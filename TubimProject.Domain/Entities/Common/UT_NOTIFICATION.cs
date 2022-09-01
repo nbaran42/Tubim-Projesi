336704326e8431649700dba6e5dc110e3db70eb8
nbaran42 <alexza42@hotmail.com>
2022-09-01 16:38:45 +0300

    tte

--- TubimProject.Domain/Entities/Common/UT_NOTIFICATION.cs
+++ TubimProject.Domain/Entities/Common/UT_NOTIFICATION.cs
@@ -1,10 +1,5 @@
 ﻿using Abp.Auditing;
 using AspNetCoreHero.Abstractions.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TubimProject.Domain.Entities.Common
 {
--- TubimProject.Domain/Entities/Logging/UT_AUDIT.cs
+++ TubimProject.Domain/Entities/Logging/UT_AUDIT.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TubimProject.Domain.Entities.Logging
+﻿namespace TubimProject.Domain.Entities.Logging
 {
     public class UT_AUDIT
     {
         public int Id { get; set; }
         public string Data { get; set; }
         public DateTime AuditDate { get; set; }
-     
     }
 }
